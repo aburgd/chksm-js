b20c8954d80b7ef0545cd422aa4da4a8a887d4b7
Alec Burgdorf <aeburgd@gmail.com>
2018-05-01 21:12:35 -0500

    move choices type to its own type in exports

--- src/exports.js
+++ src/exports.js
@@ -1,10 +1,12 @@
 // @flow
 
+type choiceArr = Array<{ title: string, value: string|number}>
+
 export type Question = {
   type: string,
   name: string,
   message: string,
   style?: string,
   initial?: string|number,
-  choices?: Array<{ title: string, value: string|number }>
+  choices?: choiceArr
 }
